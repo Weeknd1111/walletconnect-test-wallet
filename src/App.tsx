3da9059172f27424cfa1dcd0ccff0b8c1fca9f30
Jony <angyangjiong@outlook.com>
2022-10-07 00:50:25 +0800

    修复0索引的地址选中问题

--- src/App.tsx
+++ src/App.tsx
@@ -368,7 +368,7 @@ class App extends React.Component<{}> {
   public updateSession = async (sessionParams: { chainId?: number; activeIndex?: number }) => {
     const { connector, chainId, accounts, activeIndex } = this.state;
     const newChainId = sessionParams.chainId || chainId;
-    const newActiveIndex = sessionParams.activeIndex || activeIndex;
+    const newActiveIndex = typeof(sessionParams.activeIndex) !== "undefined" ? sessionParams.activeIndex : activeIndex;
     const address = accounts[newActiveIndex];
     if (connector) {
       connector.updateSession({
